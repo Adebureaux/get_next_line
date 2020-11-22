57038b574df3f8502ea1e6ca46783803bd366a95
Augustin Debureaux <adebureaux@gmail.com>
2020-11-22 15:11:03 +0100

    Remove global variable

--- get_next_line.c
+++ get_next_line.c
@@ -6,77 +6,78 @@
 /*   By: adeburea <adeburea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 21:53:59 by adeburea          #+#    #+#             */
-/*   Updated: 2020/11/18 03:17:19 by adeburea         ###   ########.fr       */
+/*   Updated: 2020/11/22 15:09:59 by adeburea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	g_save[30000];
-
-int		get_size(int c)
+int		get_size(int c, char save[])
 {
 	int i;
 
 	i = 0;
-	while (g_save[i] != c && g_save[i])
+	while (save[i] && save[i] != c)
 		i++;
-	if (g_save[i] == c)
+	if (save[i] == c)
 		return (i);
 	return (-1);
 }
 
-void	get_sub_save(int start, int len)
+void	get_sub_save(int start, char save[])
 {
 	int		i;
+	int		len;
 	char	*tmp;
 
+	len = ft_strlen(save);
 	i = 0;
-	while (start-- && g_save[i])
+	while (save[i] && start--)
 		i++;
-	tmp = ft_strndup(g_save + i + 1, len);
-	ft_strncpy(g_save, tmp, len);
+	tmp = ft_strndup(save + i + 1, len);
+	ft_strncpy(save, tmp, len);
 	free(tmp);
 }
 
-int		get_file(int fd)
+int		get_file(int fd, char save[])
 {
 	int rd;
 
 	if (read(fd, &rd, 0) == -1)
 		return (0);
 	rd = 1;
-	while (rd > 0 && get_size('\n') == -1)
+	while (rd > 0 && get_size('\n', save) == -1)
 	{
-		rd = read(fd, &g_save[ft_strlen(g_save)], BUFFER_SIZE);
+		rd = read(fd, &save[ft_strlen(save)], BUFFER_SIZE);
 	}
 	return (1);
 }
 
 int		get_next_line(int fd, char **line)
 {
-	int		len;
-	int		ret;
-	char	*dst;
+	int			len;
+	int			ret;
+	char		*dst;
+	static char	save[30000];
 
 	ret = 1;
 	dst = NULL;
-	if (!get_file(fd) || !line || BUFFER_SIZE < 1 || fd < 0)
+	if (!get_file(fd, save) || !line || BUFFER_SIZE < 1 || fd < 0)
 		return (-1);
-	if ((len = get_size('\n')) == -1)
+	if ((len = get_size('\n', save)) == -1)
 	{
-		len = ft_strlen(g_save);
+		len = ft_strlen(save);
 		ret = 0;
 	}
-	if (g_save[0])
-		dst = ft_strndup(g_save, len);
+	if (save[0])
+		dst = ft_strndup(save, len);
 	else
 	{
 		if (!(dst = malloc(1)))
 			return (-1);
 		dst[0] = '\0';
 	}
-	get_sub_save(len, ft_strlen(g_save));
+	get_sub_save(len, save);
 	*line = dst;
 	return (ret);
 }
